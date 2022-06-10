af163a5f55164244e0883532c81104c079f3b33c
John Walling <j00walling@gmail.com>
2022-06-10 14:32:52 -0400

    added gitignore

--- services/movies-service/src/main/java/com/github/klefstad_teaching/cs122b/movies/MoviesService.java
+++ services/movies-service/src/main/java/com/github/klefstad_teaching/cs122b/movies/MoviesService.java
@@ -2,6 +2,7 @@ package com.github.klefstad_teaching.cs122b.movies;
 
 import com.github.klefstad_teaching.cs122b.core.spring.SecuredStackService;
 import com.github.klefstad_teaching.cs122b.movies.config.MoviesServiceConfig;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
